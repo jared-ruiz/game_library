1febf3e86f060368a436022c9aa4475e187d4b27
Jared Ruiz <jared707@gmail.com>
2025-03-23 01:14:35 -0500

    Added styling to login and signup page.

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -14,14 +14,14 @@ function SignUp() {
   }
 
   return (
-    <div className="h-screen w-full app_bg">
-      <header className="max-w-6xl mx-auto flex items-center justify-between p-4 bg-black/40">
+    <div className="h-screen w-full flex flex-col app_bg">
+      <header className="max-w-6xl w-full mx-auto flex items-center justify-between p-4 bg-black/40">
         <Link to={"/"}>
           <p className="text-white text-2xl">Game Library</p>
         </Link>
       </header>
 
-      <div className="flex justify-center items-center mt-20 mx-3">
+      <div className="flex-1 flex justify-center items-center w-full max-w-6xl mx-auto bg-black/60 p-4">
         <div className="w-full max-w-md p-8 space-y-6 bg-black/60 rounded-lg shadow-md">
           <h1 className="text-center text-white text-2xl font-bold mb-4">
             Sign Up
