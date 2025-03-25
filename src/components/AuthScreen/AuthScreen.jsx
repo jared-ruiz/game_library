ece081062067a72429cc1e0052e58972afc1cbb9
Jared Ruiz <jared707@gmail.com>
2025-03-24 20:36:09 -0500

    Added new image to auth page.

--- src/components/AuthScreen/AuthScreen.jsx
+++ src/components/AuthScreen/AuthScreen.jsx
@@ -45,22 +45,56 @@ function AuthScreen() {
       </div>
 
       {/* separator */}
-      <div className="h-2 max-w-6xl w-full mx-auto bg-gray-600" aria-hidden="true" />
+      <div
+        className="h-2 max-w-6xl w-full mx-auto bg-gray-600"
+        aria-hidden="true"
+      />
 
       <div className="flex flex-1 justify-center items-center max-w-6xl mx-auto py-10 pr-5 pl-5 bg-black/60 text-white">
         <div className="flex max-w-6xl mx-auto items-center justify-center md:flex-row flex-col px-4 md:px-2">
           <div className="flex-1 p-2 text-center md:text-left">
-            <h2 className="font-bold text-2xl">Over 300k Games to Search From</h2>
-            <p className="text-lg">Quick and easy game search so you can find what you are looking for and get back to logging!</p>
+            <h2 className="font-bold text-2xl">
+              Over 300k Games to Search From
+            </h2>
+            <p className="text-lg">
+              Quick and easy game search so you can find what you are looking
+              for and get back to logging!
+            </p>
           </div>
           <div className="flex-1  text-center md:text-left">
-            <img src="/game-1.jpg" alt='ffxiv_image'className="rounded-2xl"/>    
-            <p className="flex mr-2 justify-end text-blue-400 font-bold">Final Fantasy XIV</p>  
+            <img src="/game-1.jpg" alt="ffxiv_image" className="rounded-2xl" />
+            <p className="flex mr-2 justify-end text-blue-400 font-bold">
+              Final Fantasy XIV
+            </p>
           </div>
         </div>
       </div>
 
-      <div className="h-2 max-w-6xl w-full mx-auto bg-gray-600" aria-hidden="true" />
+      {/* seperator */}
+      <div
+        className="h-2 max-w-6xl w-full mx-auto bg-gray-600"
+        aria-hidden="true"
+      />
+
+      <div className="flex flex-1 justify-center items-center max-w-6xl mx-auto py-10 pr-5 pl-5 bg-black/60 text-white">
+        <div className="flex max-w-6xl mx-auto items-center justify-center md:flex-row flex-col px-4 md:px-2"></div>
+        <div className="flex-1 text-center md:text-left">
+            <img src="/game-2.jpg" alt="bioshock_img" className="rounded-2xl" />
+            <p className="flex mr-2 justify-start text-blue-400 font-bold">
+              BioShock
+            </p>
+          </div>     
+        
+        <div className="flex-1 p-2 text-center md:text-left">
+            <h2 className="font-bold text-2xl ml-4">
+              Keep Track of All Your Gaming History
+            </h2>
+            <p className="text-lg ml-4">
+              Search, Save, Repeat. Your own personal digital library!
+            </p>
+          </div>
+ 
+      </div>
     </div>
   );
 }
